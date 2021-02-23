94a503dc95e5f051adb6f4073bf469d1bb1d6877
Danny Tabor <djt77@outlook.com>
2021-02-23 06:05:01 -0500

    Codewars challenges updated with new finished challenges

---
+++ training-js7/js/main.js
@@ -0,0 +1,18 @@
+/*Complete function saleHotdogs/SaleHotDogs/sale_hotdogs, function accept 1 parameters:n, n is the number of customers to buy hotdogs, different numbers have different prices (refer to the following table), return a number that the customer need to pay how much money.*/
+
+
+function saleHotdogs(n){
+    if(n < 5){
+        return n * 100
+    } else if(n >= 5 && n < 10){
+        return n * 95
+    } else if( n >= 10){
+        return n * 90
+    }
+  }
+
+  //refractored
+
+  function saleHotdogs(n){
+      return n(n<5?100:n<10?95:90);
+  }
