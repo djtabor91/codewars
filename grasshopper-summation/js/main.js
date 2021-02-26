7ebe83caeca08faa2822957c268ae38d0d4ab234
Danny Tabor <djt77@outlook.com>
2021-02-26 06:47:20 -0500

    grasshopper summation challenge added

---
+++ grasshopper-summation/js/main.js
@@ -0,0 +1,10 @@
+
+//Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+var summation = function (num) {
+    let count = 0;
+    for(let i = 0; i <= num; i++){
+      count += i
+    }
+    return count
+  }
