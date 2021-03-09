95e48bf9a9a40447f91c5317c14dc44a818ce368
Danny Tabor <djt77@outlook.com>
2021-03-09 06:30:11 -0500

    two more challenges!

--- dna-rna-conversion/js/main.js
+++ dna-rna-conversion/js/main.js
@@ -1,7 +1,10 @@
 
 function DNAtoRNA(dna) {
     // create a function which returns an RNA sequence from the given DNA sequence
-    return dna.replaceAll('T', 'U');
+    //return dna.replaceAll('T', 'U');
+    for(let i = 0; i < dna.length; i++){
+      dna = dna.replace('T', 'U');
+    }
       
 
   }
