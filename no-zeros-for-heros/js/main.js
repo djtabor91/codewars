2f37399572b4f2fe7f628882abe1a3d28f32cd11
Danny Tabor <djt77@outlook.com>
2021-04-21 10:38:58 -0400

    getting another one

---
+++ no-zeros-for-heros/js/main.js
@@ -0,0 +1,34 @@
+/*Numbers ending with zeros are boring.
+
+They might be fun in your world, but not here.
+
+Get rid of them. Only the ending ones.
+
+1450 -> 145
+960000 -> 96
+1050 -> 105
+-1050 -> -105
+Zero alone is fine, don't worry about it. Poor guy anyway */
+
+function noBoringZeros(n) {
+    // your code
+    if (n === 0) return 0
+  const number = String(n)
+  let result = number
+  for (let i = 0; i < number.length; i++) {
+    if (result.endsWith('0')) {
+      result = result.substring(0, result.length - 1)
+    }
+  }
+  return Number(result)
+}
+  
+console.log(noBoringZeros(43400000));
+
+
+//turn number into string
+//turn string into array
+//split array
+//loop through array
+//check to see if any index is === to 0
+// 
