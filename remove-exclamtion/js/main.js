0653117bf9e4f9a754aaafc559fd5fda3e42b678
Danny Tabor <djt77@outlook.com>
2021-05-13 10:15:55 -0400

    fixed solution to remove any exclamation

--- remove-exclamtion/js/main.js
+++ remove-exclamtion/js/main.js
@@ -1,10 +1,22 @@
 /*Write function RemoveExclamationMarks which removes all exclamation marks from a given string. */
 
 function removeExclamationMarks(s) {
-    while(s[s.length - 1] === '!'){
+    /*while(s[s.length - 1] === '!'){
         s = s.slice(0,-1)
     }
+    return s*/
+    /*for(let i = 0; i < s.length; i++){
+        let strAry = s.split('')
+        if(strAry[i] == '!'){
+            strAry.filter('!');
+            return strAry
+        }
+        
+    }*/
+    s = s.split('');
+    s = s.filter(e => e !== '!')
+    s= s.join('')
     return s
   }
 
-  console.log(removeExclamationMarks('Hello World!'))
+  console.log(removeExclamationMarks('Hello! World!'))
