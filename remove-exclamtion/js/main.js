eeeb71c5aab940b94144147ee78ab1422e553591
Danny Tabor <djt77@outlook.com>
2021-05-15 15:56:41 -0400

    adding a couple code wars gotta grind

--- remove-exclamtion/js/main.js
+++ remove-exclamtion/js/main.js
@@ -1,6 +1,6 @@
 /*Write function RemoveExclamationMarks which removes all exclamation marks from a given string. */
 
-function removeExclamationMarks(s) {
+/*function removeExclamationMarks(s) {
     /*while(s[s.length - 1] === '!'){
         s = s.slice(0,-1)
     }
@@ -13,9 +13,17 @@ function removeExclamationMarks(s) {
         }
         
     }*/
-    s = s.split('');
+    /*s = s.split('');
     s = s.filter(e => e !== '!')
     s= s.join('')
+    s= s.split('').filter(e => e !== '!').join('')
+    return s
+  }
+
+  console.log(removeExclamationMarks('Hello! World!'))*/
+
+  function removeExclamationMarks(s){
+    s= s.split('').filter(e => e !== '!').join('')
     return s
   }
 
