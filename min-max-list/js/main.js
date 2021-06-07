6fafc1d8a08786b09d886d98055d00271c21b0a4
Danny Tabor <djt77@outlook.com>
2021-06-07 19:13:58 -0400

    codewar push

---
+++ min-max-list/js/main.js
@@ -0,0 +1,27 @@
+/*Your task is to make two functions, max and min (maximum and minimum in PHP and Python, maxi and mini in Julia) that take a(n) array/vector of integers list as input and outputs, respectively, the largest and lowest number in that array/vector.
+
+#Examples
+
+max([4,6,2,1,9,63,-134,566]) returns 566
+min([-52, 56, 30, 29, -54, 0, -110]) returns -110
+max([5]) returns 5
+min([42, 54, 65, 87, 0]) returns 0
+#Notes
+
+You may consider that there will not be any empty arrays/vectors. */
+
+var min = function(list){
+    
+    list = list.sort((a,b) => a-b);
+    return list[0]
+}
+
+var max = function(list){
+
+    list = list.sort((a,b) => a - b)
+    return list[list.length - 1]
+    
+}
+
+console.log(min([-52, 56, 30, 29, -54, 0, -110]))
+console.log(max([4,6,2,1,9,63,-134,566]))
