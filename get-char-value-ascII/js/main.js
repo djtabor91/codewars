101da4e5fdc1a59560e96ad2411b8f7e89e736c0
Danny Tabor <djt77@outlook.com>
2021-06-09 12:50:36 -0400

    adding another challenge

---
+++ get-char-value-ascII/js/main.js
@@ -0,0 +1,11 @@
+
+/*Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+
+get_char(65) # => 'A' */
+function getChar(c){
+    return String.fromCharCode(c)    
+}
+
+  console.log(getChar(2))
