ec49fdca14646d6d2a19bcb10c36fee200dae0ea
Danny Tabor <djt77@outlook.com>
2021-07-27 07:01:58 -0400

    7 kyu update

---
+++ even-times-last/js/main.js
@@ -0,0 +1,40 @@
+/*Given a sequence of integers, return the sum of all the integers that have an even index, multiplied by the integer at the last index.
+
+Indices in sequence start from 0.
+
+If the sequence is empty, you should return 0. */
+
+function evenLast(numbers) {
+    // Good luck
+    let last = numbers[numbers.length -1]
+    let sum = 0;
+    for(let i = 0; i < numbers.length; i++){
+        if( i % 2 == 0){
+            sum += numbers[i]
+        }
+    }
+    return sum * last
+}
+
+console.log(evenLast([2, 3, 4, 5]))
+console.log(evenLast([2, 2, 2, 2]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
