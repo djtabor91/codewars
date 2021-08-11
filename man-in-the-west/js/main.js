9709123d9acbe1cef5613d1c5bfbba013e7c0216
Danny Tabor <djt77@outlook.com>
2021-08-11 19:15:15 -0400

    adding challenge

---
+++ man-in-the-west/js/main.js
@@ -0,0 +1,25 @@
+/*A western man is trying to find gold in a river. To do that, he passes a bucket through the river's soil and then checks if it contains any gold. However, he could be more productive if he wrote an algorithm to do the job for him.
+
+So, you need to check if there is gold in the bucket, and if so, return True/true. If not, return False/false. */
+
+function checkTheBucket(bucket){
+    //your code here
+    /*for(let i = 0; i < bucket.length; i++){
+        if( bucket[i]=== 'gold'){
+            return true;
+        }else{
+            return false
+        }
+    }*/
+    /*bucket = bucket.filter(g => g == 'gold');
+    if(bucket == 'gold'){
+        return true;
+    }else{
+        return false
+    }*/
+    return bucket.includes('gold');
+    
+}
+
+console.log(checkTheBucket(["stone", "stone", "stone", "stone", "gold"]))
+console.log(checkTheBucket(["stone", "stone", "stone", "gold", "gold"]))
